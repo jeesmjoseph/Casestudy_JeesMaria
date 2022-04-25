8d9c64d9ed238c86bf8c33df9c9a0d45aa5212c1
jeesmjoseph <78970300+jeesmjoseph@users.noreply.github.com>
2022-04-25 16:10:09 +0530

    Revert "Version_1.3"
    This reverts commit 3f2132c213cc67d5293277c4d8b45ce2b27b335f.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-
+document.addEventListener('DOMContentLoaded',function () {
 
 	const dbUrl = 'https://jsonplaceholder.typicode.com/todos';
 	let list = document.getElementById('base');
-	
+	let id_arr = [];
 
 	function getAllTodos() {
         return new Promise(resolve => {
@@ -13,20 +13,14 @@
     }
 
 	function appendTextToList(todo) {
-		if(todo.completed){
-			list.innerHTML += `
-			<input type="checkbox" id ="${todo.id}" checked disabled>
-			<label style="padding-left : 20px; opacity : 0.5; text-decoration: line-through;">${todo.title}</label>
-			<hr>	
+        list.innerHTML += `
+			<input type="checkbox" id ="${todo.id}">
+			${todo.title}
+			<br>	
 		`;
-		}
-		else{
-			list.innerHTML += `
-			<input type="checkbox" id ="${todo.id}" onChange ="cbChange();">
-			<label style="padding-left : 20px">${todo.title}</label>
-			<hr>	
-		`;
-		}
+		if (todo.completed){
+			id_arr.push(todo.id);
+		} 
     }
 
 	async function asyncall(){
@@ -36,20 +30,6 @@
 	}
 	
 	asyncall();
-
-	var count = 0;
-	function cbChange(){
-			if(this.checked == true)
-			{
-				count +=1;
-			}
-			if (count === 5)
-			{
-				alert("Congrats. 5 Tasks have been Successfully Completed !");
-			}	
-		}
-
-
-
-
+	
+})
 
